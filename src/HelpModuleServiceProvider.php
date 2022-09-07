261ace02cd4fa42c264277be3d414db597c67a1d
Pradeep-sati12 <Pradeep-sati12@users.noreply.github.com>
2022-09-07 09:19:44 +0000

    Fix styling

--- src/HelpModuleServiceProvider.php
+++ src/HelpModuleServiceProvider.php
@@ -2,14 +2,12 @@
 
 namespace HelpModule\HelpModule;
 
+use App\Models\HelpingList;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use HelpModule\HelpModule\Commands\HelpModuleCommand;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
-use App\Models\HelpingList;
-use Illuminate\Http\Request;
+
 class HelpModuleServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
@@ -23,47 +21,37 @@ class HelpModuleServiceProvider extends PackageServiceProvider
             ->name('help-module')
             ->hasViews()
             ->hasMigration('create_help-module_table');
-            
     }
+
     public function boot()
     {
         View::composer('*', function ($view) {
-            
-            
-            $currentURL=  url()->full();
-            $helptext = "";
+            $currentURL = url()->full();
+            $helptext = '';
             $contains = Str::contains($currentURL, 'help=technical');
-            if($contains)
-            {
-                //storing the substring before the query string in $slice	
+            if ($contains) {
+                //storing the substring before the query string in $slice
                 $slice = substr($currentURL, 0, strpos($currentURL, '&help=technical'));
-                
-            //fetching the records from the database
-    
-                $urls = HelpingList::where('page_url', $slice)->where('status','1')->first();
-                
 
-             if(empty($urls)){
+                //fetching the records from the database
 
-                $slice = substr($currentURL, 0, strpos($currentURL, '?'));
-    
-                //HelpingList is the model  we will match the url.
+                $urls = HelpingList::where('page_url', $slice)->where('status', '1')->first();
 
-                $urls = HelpingList::where('page_url', $slice)->where('status','1')->first();
-                
-             }
+                if (empty($urls)) {
+                    $slice = substr($currentURL, 0, strpos($currentURL, '?'));
 
-            }
-            else{
-                $urls="";
-                $currenturl= url()->current();
-             
-                $helptext = HelpingList::where('page_url', $currenturl)->where('status','1')->first();
-                
+                    //HelpingList is the model  we will match the url.
+
+                    $urls = HelpingList::where('page_url', $slice)->where('status', '1')->first();
+                }
+            } else {
+                $urls = '';
+                $currenturl = url()->current();
+
+                $helptext = HelpingList::where('page_url', $currenturl)->where('status', '1')->first();
             }
 
-            $view->with('urls', $urls)->with('helpurl',$helptext);
-
+            $view->with('urls', $urls)->with('helpurl', $helptext);
         });
     }
 }
